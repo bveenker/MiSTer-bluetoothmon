f101e34fe83bc2717f89636a5d79aab8b91878a0
bveenker <brian@mech.us>
2026-07-26 15:10:34 -0700

    Create bluetoothmon.sh

---
+++ Scripts/bluetoothmon.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+get_connected_bt_devices() {
+    local count=0
+
+    while read -r _ mac _; do
+        if bluetoothctl info "$mac" | grep -q "Connected: yes"; then
+            ((count++))
+        fi
+    done < <(bluetoothctl devices)
+
+    echo "$count"
+}
+
+install() {
+  if ! grep -q "# bveenker/bluetoothmon" /media/fat/linux/user-startup.sh ; then
+    echo Installing ${0##*/}... >> /tmp/${0##*/}.log
+    echo '
+# bveenker/bluetoothmon
+[[ -e /media/fat/Scripts/bluetoothmon.sh ]] && /media/fat/Scripts/bluetoothmon.sh $1 &' >> /media/fat/linux/user-startup.sh
+  fi
+
+}
+
+start() {
+  echo Starting ${0##*/}... >> /tmp/${0##*/}.log
+
+  echo "$$" > /tmp/${0##*/}.pid
+  
+  total_connected=0
+  while true; do
+	
+	echo reset bluetooth >> /tmp/${0##*/}.log
+	bluetoothctl power off
+	sleep 1
+	bluetoothctl power on
+
+	echo there are no bluetooth devices. wait until there is one >> /tmp/${0##*/}.log
+    while [ $total_connected -eq "0" ]; do
+      sleep 10
+	  total_connected=$(get_connected_bt_devices)
+    done
+	
+	echo there is at least one connected. wait until there are none  >> /tmp/${0##*/}.log
+    while [ $total_connected -ne "0" ]; do
+      sleep 10
+	  total_connected=$(get_connected_bt_devices)
+    done
+
+	echo there are no bluetooth devices >> /tmp/${0##*/}.log
+	
+  done
+  
+}
+
+stop() {
+  echo Stopping ${0##*/}... >> /tmp/${0##*/}.log
+  
+  if [[ -f /tmp/${0##*/}.pid ]]; then
+    kill "$(cat /tmp/${0##*/}.pid)"
+    rm -f /tmp/${0##*/}.pid
+  fi
+}
+
+restart() {
+  echo Restarting ${0##*/}... >> /tmp/${0##*/}.log
+  stop
+  ${0} start &
+}
+
+main() {
+  install
+  "${1:-start}"
+}
+
+main "${@}"
